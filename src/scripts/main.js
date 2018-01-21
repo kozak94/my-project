ec49955fa159713110148df1f6f318f26f3c0d71
kozak94 <Muhaylo.Gandjyk@gmail.com>
2018-01-22 00:19:45 +0200

    МаксТемп виводиться в Цельсіях

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,18 +11,44 @@
         url: apiAdress,
         dataType: 'json',
         success: function (objData) {
-          console.log(objData);
+          console.log(objData);// побачили всю відповідь з сервера
+          var DailyForecasts=  objData.DailyForecasts;
+          console.log(DailyForecasts);// достали блок з температурою
+          for (var i in DailyForecasts){
+            console.log(DailyForecasts[i].Temperature);// достали Макс і Мін температури
+          };
+          var Temperature = DailyForecasts[i].Temperature;
+            for(var a in Temperature){
+              console.log(Temperature[a]); // виводим два обєкта Макс і Мін температури             
+            }
+          TemperatureArr = Temperature[a];// забираємо останій обєкт (максимальну температуру) Питання як забрати першу? 
+          console.log(TemperatureArr);
+          
+            var MaxTemperature = TemperatureArr.Value;// передаємо  зі значенням(Value) обєктa макс температури 
+            console.log(MaxTemperature);
+          
+            for (var i = MaxTemperature; i <= +MaxTemperature ; i++) {// функція для переводу ференгейти в цельсії
+              var MaxTemperatureC = Math.round((i - 32) * (5 / 9));
+            }
 
-          for (var i in objData) {
-            console.log(i);
-            var headline = objData[i];
-            console.log(headline[0]); // як достати з цього масиву значення( температуру і т.д.)
+
+          $('#MaxTemp').text(MaxTemperatureC); // виводимо в Ферингейтах температуру.
+
+     
 
 
-            $(document.body).append("<div><p>" + headline + " </p>" + "</div>");
-            console.log(i + objData);
+
+
+          // for (var i in objData) {
+          //   console.log(i);
+          //   var headline = objData[i];
+          //   console.log(headline[0]); // як достати з цього масиву значення( температуру і т.д.)
+          //   var headlineArr = headline[0];
+          //   console.log(headlineArr);
+
+          
           }
-        }
+        // }
 
       }).then(function (data) {
         console.log(data);
