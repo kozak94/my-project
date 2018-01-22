9277f2209597a244af19306eb1cf179192d5bb16
kozak94 <Muhaylo.Gandjyk@gmail.com>
2018-01-22 22:47:38 +0200

    added more info

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,8 @@
       var apiAdress =
         'http://dataservice.accuweather.com/forecasts/v1/daily/1day/324505?apikey=AVxnqMfPACom2FnCptPjufAcGCiKMLkx&details=true';
       var value = $("#txt_name").val();// cюди добавлю скріпт для пошуку міст відповідно до того що ввів користувач
-      console.log(value);
+
+      // даємо запит до АРІ по отриманій локації і виводимо значення макс і мін температури тиск і шв.вітру
       $.ajax({
         type: 'GET',
         url: apiAdress,
@@ -18,21 +19,48 @@
             console.log(DailyForecasts[i].Temperature);// достали Макс і Мін температури
           };
           var Temperature = DailyForecasts[i].Temperature;
+
             for(var a in Temperature){
               console.log(Temperature[a]); // виводим два обєкта Макс і Мін температури             
             }
-          TemperatureArr = Temperature[a];// забираємо останій обєкт (максимальну температуру) Питання як забрати першу? 
-          console.log(TemperatureArr);
+          var TemperatureMin = Temperature.Minimum;//забираємо мінімальну температуру.
+          console.log(TemperatureMin);
+          TemperatureMax = Temperature.Maximum;// забираємо максимальну температуру.
+          console.log(TemperatureMax);
           
-            var MaxTemperature = TemperatureArr.Value;// передаємо  зі значенням(Value) обєктa макс температури 
+            var MaxTemperature = TemperatureMax.Value;// передаємо значення(Value) обєктa макс температури 
             console.log(MaxTemperature);
-          
+            var MinTemperature = TemperatureMin.Value; // передаємо значення(Value) обєктa макс температури
+            console.log(MinTemperature);
+
             for (var i = MaxTemperature; i <= +MaxTemperature ; i++) {// функція для переводу ференгейти в цельсії
               var MaxTemperatureC = Math.round((i - 32) * (5 / 9));
             }
+            for (var i = MinTemperature; i <= +MinTemperature; i++) { // функція для переводу ференгейти в цельсії
+              var MinTemperatureC = Math.round((i - 32) * (5 / 9));
+            }
+          
+          // виводимо Швидкість вітру
+          for (var i in DailyForecasts) {
+            console.log(DailyForecasts[i].Day); // Достали масив обэктів в якому є Вітер
+          };
+
+          var Wind = DailyForecasts[i].Day;
+          
+          var WindSpeedM = Wind.Wind.Speed.Value;// отримали шв вітру в милях
+          for (var i = WindSpeedM; i <= +WindSpeedM; i++) { // функція для переводу ференгейти в цельсії
+
+            var WindSpeedKm = Math.round(i / 0.625 );
+
+          }
+          console.log(WindSpeedKm);
+
+          console.log(objData.DailyForecasts["0"].Temperature.Maximum.Value);// РІШЕННЯ УСІХ ПРОБЛЕМ!!!
 
 
           $('#MaxTemp').text(MaxTemperatureC); // виводимо в Цельсіях температуру.
+          $('#MinTemp').text(MinTemperatureC); 
+          $('#WindSpeed').text(WindSpeedKm);
 
      
 
