694c36e00fb0675ceb1db6880ffbc20e49082079
kozak94 <Muhaylo.Gandjyk@gmail.com>
2018-01-24 23:11:49 +0200

    add City Key from input

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,26 @@
 $(document).ready(function () {
   var apiKey = 'AVxnqMfPACom2FnCptPjufAcGCiKMLkx'
   $("button").click(function () {
+    var CityInput = $('#City').val();
+    console.log(CityInput);
+    var url = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=AVxnqMfPACom2FnCptPjufAcGCiKMLkx&q="
+    var APIurl = url + CityInput ;
+    console.log(APIurl);
+    var CityKey = 0
+    $.ajax({
+      async: false,// танці з бубном для передачі значеннян локальної зміної CityKey в глобальну
+      type: 'GET',
+      url: url + CityInput,
+      dataType: 'json',
+      success: function (CityData) {
+        console.log(CityData);
+        CityKey = CityData['0'].Key;
+        console.log(CityKey);
+ 
+      }
+      
+    })
+    console.log(CityKey);
     
 
 
@@ -8,8 +28,8 @@ $(document).ready(function () {
 
 
     var apiAdress =
-      'http://dataservice.accuweather.com/forecasts/v1/daily/1day/324505?apikey=AVxnqMfPACom2FnCptPjufAcGCiKMLkx&details=true';
-    var value = $("#txt_name").val(); // cюди добавлю скріпт для пошуку міст відповідно до того що ввів користувач
+      'http://dataservice.accuweather.com/forecasts/v1/daily/1day/' +CityKey +'?apikey=AVxnqMfPACom2FnCptPjufAcGCiKMLkx&details=true';
+    console.log(apiAdress);
 
     // даємо запит до АРІ по отриманій локації і виводимо значення макс і мін температури тиск і шв.вітру
     $.ajax({
