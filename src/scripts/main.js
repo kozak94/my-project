377a749fc7deb265ccde2bbb411e58ff66a55585
kozak94 <Muhaylo.Gandjyk@gmail.com>
2018-01-26 20:32:08 +0200

    added some func

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,8 @@ $(document).ready(function () {
     var url = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=AVxnqMfPACom2FnCptPjufAcGCiKMLkx&q="
     var APIurl = url + CityInput ;
     console.log(APIurl);
-    var CityKey = 0
+    // var CityKey = 0;
+    // var CityName = 0;
     $.ajax({
       async: false,// танці з бубном для передачі значеннян локальної зміної CityKey в глобальну
       type: 'GET',
@@ -16,19 +17,17 @@ $(document).ready(function () {
         console.log(CityData);
         CityKey = CityData['0'].Key;
         console.log(CityKey);
- 
+        CityName = CityData['0'].EnglishName;
+        console.log(CityName);
+        $('#City').text(CityName);
+
       }
       
     })
     console.log(CityKey);
-    
-
-
-
-
-
+    console.log(CityName); $('#City').text(CityName);
     var apiAdress =
-      'http://dataservice.accuweather.com/forecasts/v1/daily/1day/' +CityKey +'?apikey=AVxnqMfPACom2FnCptPjufAcGCiKMLkx&details=true';
+      'http://dataservice.accuweather.com/currentconditions/v1/' + CityKey + '?apikey=AVxnqMfPACom2FnCptPjufAcGCiKMLkx&details=true';
     console.log(apiAdress);
 
     // даємо запит до АРІ по отриманій локації і виводимо значення макс і мін температури тиск і шв.вітру
@@ -38,30 +37,46 @@ $(document).ready(function () {
       dataType: 'json',
       success: function (objData) {
         console.log(objData); 
-        var MaxTemperature = objData.DailyForecasts["0"].Temperature.Maximum.Value;
-        for (var i = MaxTemperature; i <= +MaxTemperature; i++) { // функція для переводу ференгейти в цельсії
-          var MaxTemperatureC = Math.round((i - 32) * (5 / 9));
-        }
-        $('#MaxTemp').text(MaxTemperatureC + '	C');
-        var MinTemperature = objData.DailyForecasts["0"].Temperature.Minimum.Value;
-        for (var i = MinTemperature; i <= +MinTemperature; i++) { // функція для переводу ференгейти в цельсії
-          var MinTemperatureC = Math.round((i - 32) * (5 / 9));
-        }
-        $('#MinTemp').text(MinTemperatureC +'	C');
+
+        var NowTemp = objData["0"].ApparentTemperature.Metric.Value
+        $('#NowTemp').text(NowTemp);
+        console.log(NowTemp);
+
+        var WindSpeed = objData["0"].Wind.Speed.Metric.Value; 
+          console.log(WindSpeed);
+        $('#WindSpeed').text(WindSpeed + ' km/h');
+
+        var Pressure = objData["0"].Pressure.Metric.Value;
+        console.log(WindSpeed);
+        $('#Pressure').text(Pressure + ' мм. рт. ст.');
+        
+        var MaxTemperature = objData["0"].TemperatureSummary.Past24HourRange.Maximum.Metric.Value;
+        $('#MaxTemp').text(MaxTemperature + '	C');
+
+        var MinTemperature = objData["0"].TemperatureSummary.Past24HourRange.Minimum.Metric.Value;
+        $('#MinTemp').text(MinTemperature +'	C');
+       
+        
+
+
 
         
-        var WindSpeedM = objData.DailyForecasts["0"].Day.Wind.Speed.Value; // отримали шв вітру в милях
-        for (var i = WindSpeedM; i <= +WindSpeedM; i++) { // функція для переводу миль\год в км\год
-          var WindSpeedKm = Math.round(i / 0.625);
-          console.log(WindSpeedKm);
-        }
-        $('#WindSpeed').text(WindSpeedKm + ' km/h');
-          
+        
+        
       }
       
 
     }).then(function (data) {
       console.log(data);
     });
-  });
+    var GoogleApiAdress = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDVdtwUAOqbtpbKDAB0OA-9iCkAAcsZJCc"
+    $.ajax({
+      type: 'POST',
+      url: GoogleApiAdress,
+      dataType: 'json',
+      success: function (GeoData){
+        console.log(GeoData);
+      }
+    });
 });
+  });
---
+++ src/scripts/map.js
@@ -0,0 +1,18 @@
+$(document).ready(   function initMap() {
+    
+    var uluru = { lat: 50.456, lng: 30.520 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    }); 
+    
+});
+// Try HTML5 geolocation.
+    
+
+
+     
